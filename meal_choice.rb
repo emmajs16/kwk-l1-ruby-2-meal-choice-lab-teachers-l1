08e66242e351e7147dfc70092e25f29d5e7e9f4f
Emma Stoverink <emmajs16@live.com>
2018-06-05 17:13:48 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -22,7 +22,7 @@ puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
 
 # call your methods here
-puts breakfast("waffles")
+puts breakfast("Frosted Flakes")
 puts lunch("pizza")
 puts dinner("chicken")
 
